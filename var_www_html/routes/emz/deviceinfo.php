7a85d4b55cdb6340773139bf8fc6f60ba3783638
Simon Hofmeister <simon1.hofmeister@st.oth-regensburg.de>
2020-01-16 10:58:40 +0100

    updated-key-mgmt

--- var_www_html/routes/emz/deviceinfo.php
+++ var_www_html/routes/emz/deviceinfo.php
@@ -52,7 +52,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
-    accessToken: leaflet-api-key
+    accessToken: <?php>echo $leaflet-api-key;<?>
 }).addTo(binOverviewMap);
 // timeout in actual version of leaflet needed to show map in full area
 setTimeout(function(){ binOverviewMap.invalidateSize()}, 50);
